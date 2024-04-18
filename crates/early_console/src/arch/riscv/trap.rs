d4c85543ff1a4a8780b156566400d1728a4ae0e2
Shi Lei <shi_lei@massclouds.com>
2024-04-18 08:26:10 +0800

    early_console: remove useless code

--- crates/early_console/src/arch/riscv/trap.rs
+++
@@ -1,8 +0,0 @@
-use crate::trap::TRAPFRAME_SIZE;
-
-include_asm_marcos!();
-
-core::arch::global_asm!(
-    include_str!("trap.S"),
-    trapframe_size = const TRAPFRAME_SIZE,
-);
--- crates/early_console/src/trap.rs
+++
@@ -1,6 +0,0 @@
-//! Trap handling.
-
-use crate::arch::TrapFrame;
-
-pub const TRAPFRAME_SIZE: usize = core::mem::size_of::<TrapFrame>();
-pub const STACK_ALIGN: usize = 16;
